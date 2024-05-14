6ddef7aa24c485843a1aef75b2d1036e5d90f283
Sophie Langdon <sophie.langdon27@gmail.com>
2024-05-14 21:38:47 +0100

    Created full double chirp and tone to send for testing

--- kevin.py
+++ kevin.py
@@ -2,29 +2,28 @@ import numpy as np
 from scipy.signal import chirp
 import sounddevice as sd
 
-# Parameters
-duration = 5.0  # seconds
-frequency = 22000.0  # Hz (A4 note)
+# Tone Parameters
+tone_duration = 1.0  # seconds
+tone_frequency = 1000.0  # Hz 
 sample_rate = 44100  # samples per second
 
-# Generate a NumPy array with the audio data (sine wave)
-# t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
-# audio_data = 0.5 * np.sin(2 * np.pi * frequency * t)
-# audio_data = np.random.normal(-1, 1, int(sample_rate * duration))
+# Generate a tone to check our chirp detection is correct. 
+t_tone = np.linspace(0, tone_duration, int(sample_rate * tone_duration), endpoint=False)
+tone = 0.5 * np.sin(2 * np.pi * tone_frequency * t_tone)
 
-total_t = 2
 
+# Chirp Parameters
+chirp_t = 2
 start_sig = [0]*sample_rate
 
-t = np.linspace(0, total_t, total_t*sample_rate)
-chirp_sig = chirp(t, f0=100, f1=1000, t1=total_t, method='logarithmic')
-print(len(chirp_sig)/sample_rate)
+t = np.linspace(0, chirp_t, chirp_t*sample_rate)
+chirp_sig = chirp(t, f0=0.1, f1=22050, t1=chirp_t, method='linear')
 chirp_sig = list(chirp_sig)
-chirp_sig.extend(chirp_sig*2)
-print(len(chirp_sig)/sample_rate)
+chirp_sig.extend(chirp_sig)  # Generates a second chirp 
 
-start_sig.extend(chirp_sig)
+overall_sig = start_sig.extend(chirp_sig)  # Adds the 1s of nothing at the start to the double chirp
+overall_sig.extend(tone)  # Adds the tone to the end of the signal so we can check if our detection is correct. 
 
 # Play the audio data
-sd.play(start_sig, sample_rate)
+sd.play(overall_sig, sample_rate)
 sd.wait()  # Wait until the sound has finished playing
