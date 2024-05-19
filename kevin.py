78bd70eb958abb56d6c8f1a05bab658f5b94e03a
Sophie Langdon <sophie.langdon27@gmail.com>
2024-05-19 16:24:14 +0100

    Seperate file for synchronisation using channel estimation

--- kevin.py
+++ kevin.py
@@ -17,7 +17,7 @@ tone = tone_prefix + tone
 
 
 # Chirp Parameters
-chirp_duration = 60  # seconds
+chirp_duration = 0.5  # seconds
 start_sig = [0]*sample_rate  # 1 second silence
 half_s_samples = int(0.5*sample_rate) #generates half a second of samples 
 # gap = [0]*half_s_samples
@@ -25,21 +25,18 @@ start_freq = 0.01
 end_freq = 22050
 chirp_type = "linear" 
 
-t = np.linspace(0, chirp_duration, chirp_duration*sample_rate)  # time-values for chirp
+t = np.linspace(0, chirp_duration, int(chirp_duration*sample_rate))  # time-values for chirp
 chirp_sig = chirp(t, f0=start_freq, f1=end_freq, t1=chirp_duration, method=chirp_type)
 chirp_sig = list(chirp_sig) 
 chirp_prefix = chirp_sig[-cyclic_prefix:]
-chirp_w_prefix = chirp_prefix + chirp_sig
-#data = [0.8]*44100
-#chirp_w_prefix.extend(chirp_w_prefix)  # Generates a second chirp 
+chirp_suffix = chirp_sig[:cyclic_prefix]
+chirp_w_prefix_suffix = chirp_prefix + chirp_sig + chirp_suffix
 
-overall_sig = start_sig + chirp_w_prefix + tone# + gap  # Adds the 1s of nothing at the start to the chirp with the prefix then 
+overall_sig = start_sig + chirp_w_prefix_suffix * 5 + tone# + gap  # Adds the 1s of nothing at the start to the chirp with the prefix then 
                                                        # a small gap and then the tone. 
 
+# print(overall_sig)
 
 # Play the audio data
 sd.play(overall_sig, sample_rate)
 sd.wait()  # Wait until the sound has finished playing
-
-plt.plot(tone[:5500])
-plt.show()
