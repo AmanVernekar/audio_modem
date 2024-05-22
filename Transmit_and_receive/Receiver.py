a5bc595f30f60404388651acec4efd97f337a9f5
Sophie Langdon <sophie.langdon27@gmail.com>
2024-05-22 15:41:00 +0100

    Added using a saved recording, added plots, added shifting functions

--- Transmit_and_receive/Receiver.py
+++ Transmit_and_receive/Receiver.py
@@ -3,6 +3,8 @@ from numpy.fft import fft, ifft
 import matplotlib.pyplot as plt
 import sounddevice as sd
 from scipy.signal import chirp, correlate
+from scipy.interpolate import make_interp_spline
+from scipy.ndimage import gaussian_filter1d
 
 
 datachunk_len = 2048
@@ -11,7 +13,7 @@ symbol_len = datachunk_len + prefix_len
 lower_freq = 1000
 upper_freq = 11000
 sample_rate = 44100  # samples per second
-duration = 5
+duration = 6
 chirp_duration = 2
 chirp_start_freq = 0.01
 chirp_end_freq = 22050
@@ -36,29 +38,61 @@ def calculate_bins(sample_rate, lower_freq, upper_freq, ofdm_chunk_length):
 lower_bin, upper_bin = calculate_bins(sample_rate, lower_freq, upper_freq, datachunk_len)
 
 t_total = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
-t_chirp = np.linspace(0, chirp_duration, int(sample_rate * chirp_duration), endpoint=False)
+t_chirp = np.linspace(0, chirp_duration/2, int(sample_rate * chirp_duration), endpoint=False)
 
 chirp_sig = chirp(t_chirp, f0=chirp_start_freq, f1=chirp_end_freq, t1=chirp_duration, method=chirp_type)
 chirp_sig = list(chirp_sig)
 
-recording = sd.rec(sample_rate*duration, samplerate=sample_rate, channels=1, dtype='int16')
-sd.wait()
 
-# step 2: perform channel estimation and synchronisation steps
+# # Using real recording 
+# recording = sd.rec(sample_rate*duration, samplerate=sample_rate, channels=1, dtype='int16')
+# sd.wait()
 
-# Apply the matched filter for synchronisation
-recording = recording.flatten()  # Flatten to 1D array if necessary
-matched_filter_output = correlate(recording, chirp_sig, mode='same')
+# # # # step 2: perform channel estimation and synchronisation steps
 
-plt.plot(matched_filter_output)
+# # # # Apply the matched filter for synchronisation
+# recording = recording.flatten()  # Flatten to 1D array if necessary
+# np.save("recording_to_test_with.npy", recording)
+
+# Using saved recording
+recording = np.load("recording_to_test_with.npy")
+
+# The matched_filter_output has been changed to full as this is the default method and might be easier to work with
+# this means that the max index detected is now at the end of the chirp
+matched_filter_output = correlate(recording, chirp_sig, mode='full')
+
+# Create plots of the recording and matched filter response note that the x axes are different. 
+t_rec = np.arange(0, len(recording))
+t_mat = np.arange(0, len(matched_filter_output))
+fig, (ax1, ax2) = plt.subplots(2, 1)
+
+ax1.plot(t_rec, recording, label='Recording', color='b')
+ax1.set_xlabel('X-axis 1')
+ax1.set_ylabel('Y-axis 1')
+ax1.legend()
+ax1.set_title('First Plot')
+
+ax2.plot(t_mat, matched_filter_output, label='Matched filter output', color='r')
+ax2.set_xlabel('X-axis 2')
+ax2.set_ylabel('Y-axis 2')
+ax2.legend()
+ax2.set_title('Second Plot')
+
+plt.tight_layout()
+
+# plt.plot(abs(matched_filter_output))
 plt.show()
 
 detected_index = np.argmax(matched_filter_output)
+print(detected_index)
 
-#Use matched filter to take out the chirp from the recording
+# Use matched filter to take out the chirp from the recording
 chirp_fft = fft(chirp_sig)
-n = int(sample_rate*chirp_duration/2)
-detected_chirp = recording[detected_index-n:detected_index+n]
+# n = int(sample_rate*chirp_duration/2)
+# detected_chirp = recording[detected_index-n:detected_index+n]
+n = int(sample_rate*chirp_duration)
+print(n)
+detected_chirp = recording[detected_index-n:detected_index]
 detected_fft = fft(detected_chirp)
 channel_fft = detected_fft/chirp_fft
 channel_impulse = ifft(channel_fft)
@@ -66,16 +100,16 @@ channel_impulse = ifft(channel_fft)
 plt.plot(abs(channel_impulse))
 plt.show()
 
-def impulse_start(channel_impulse):   
+def impulse_start_10_90_jump(channel_impulse):   
     channel_impulse_max = np.max(channel_impulse)
     channel_impulse_10_percent = 0.1 * channel_impulse_max
-    channel_impulse_90_percent = 0.5 * channel_impulse_max
+    channel_impulse_90_percent = 0.6 * channel_impulse_max
 
     impulse_start = 0
 
     for i in range(len(channel_impulse) - 1):
-        if channel_impulse[i] < channel_impulse_10_percent and channel_impulse[i + 1] > channel_impulse_90_percent:
-            impulse_start = i + 1
+        if channel_impulse[i] < channel_impulse_10_percent and channel_impulse[i + 5] > channel_impulse_90_percent:
+            impulse_start = i + 5
             break
 
     if impulse_start > len(channel_impulse) / 2:
@@ -83,35 +117,84 @@ def impulse_start(channel_impulse):
 
     return impulse_start
 
-impulse_shift = impulse_start(channel_impulse)
+
+def impulse_start_max(channel_impulse):
+    impulse_start = np.argmax(channel_impulse)
+    if impulse_start > len(channel_impulse) / 2:
+        impulse_start = impulse_start - len(channel_impulse)
+
+    return impulse_start
+
+
+def impulse_start_smoothing(channel_impulse):
+
+    # Dataset
+    x = np.arange(0, len(channel_impulse), 1)
+    y = abs(channel_impulse)
+
+    X_Y_Spline = make_interp_spline(x, y)
+
+    # Returns evenly spaced numbers
+    # over a specified interval.
+    X_ = np.linspace(x.min(), x.max(), len(channel_impulse))
+    Y_ = X_Y_Spline(X_)
+
+    # Plotting the Graph
+    plt.plot(X_, Y_)
+    plt.title("Plot Smooth Curve Using the scipy.interpolate.make_interp_spline() Class")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.show()
+
+
+def impulse_start_guassian(channel_impulse): 
+    data = channel_impulse
+
+    # Apply Gaussian smoothing
+    # sigma determines the standard deviation of the Gaussian kernel
+    # window size +- 3 data points can be interpreted as sigma = 1 for a standard Gaussian kernel
+    smoothed_data = gaussian_filter1d(data, sigma=1)
+
+    print("Original Data: ", data)
+    print("Smoothed Data: ", smoothed_data)
+
+impulse_shift = impulse_start_max(channel_impulse)
+
 
 #Recalculate the section of chirp we want
-detected_chirp = recording[detected_index-n+impulse_shift:detected_index+n+impulse_shift]
+detected_chirp = recording[detected_index-n-impulse_shift:detected_index-impulse_shift]
 detected_fft = fft(detected_chirp)
 channel_fft = detected_fft/chirp_fft
 channel_impulse = ifft(channel_fft)
+
+channel_impulse_cut = channel_impulse[:2048]
+channel_coefficients = fft(channel_impulse_cut)
 
 plt.plot(abs(channel_impulse))
 plt.show()
 
 # step 2: crop audio file to the data
-data_start_index = detected_index+n+impulse_shift
+data_start_index = detected_index+impulse_shift
 recording_without_chirp = recording[data_start_index : data_start_index+recording_data_len]
 # load in the file sent to test against
-source_mod_seq = np.load("../mod_seq.npy")
+source_mod_seq = np.load("mod_seq.npy")
+print(len(source_mod_seq))
 
 
 # step 3: cut into different blocks and get rid of cyclic prefix
 
 num_symbols = int(len(recording_without_chirp)/symbol_len)  # Number of symbols 
 
+print(f"Num of OFDM symbols: {num_symbols}")
+
 time_domain_datachunks = np.array(np.array_split(recording_without_chirp, num_symbols))[:, prefix_len:]
 
 ofdm_datachunks = fft(time_domain_datachunks)  # Does the fft of all symbols individually 
-ofdm_datachunks = ofdm_datachunks/channel_fft  # Divide each value by its corrosponding channel fft coefficient. 
+ofdm_datachunks = ofdm_datachunks/channel_coefficients # Divide each value by its corrosponding channel fft coefficient. 
 data = ofdm_datachunks[:, lower_bin:upper_bin+1] # Selects the values from 1 to 511
 
 data = data.flatten()
+data = data[:len(source_mod_seq)]
 
 colors = np.where(source_mod_seq == 1+1j, "b", 
             np.where(source_mod_seq == -1+1j, "c", 
--- kevin.py
+++ kevin.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.signal import chirp
 import sounddevice as sd
+import soundfile as sf
 import matplotlib.pyplot as plt
 
 # Tone Parameters
@@ -25,9 +26,11 @@ start_freq = 0.01
 end_freq = 22050
 chirp_type = "linear" 
 
-t = np.linspace(0, chirp_duration, int(chirp_duration*sample_rate))  # time-values for chirp
+t = np.linspace(0, chirp_duration/2, int(chirp_duration*sample_rate))  # time-values for chirp
 chirp_sig = chirp(t, f0=start_freq, f1=end_freq, t1=chirp_duration, method=chirp_type)
 chirp_sig = list(chirp_sig) 
+
+
 chirp_prefix = chirp_sig[-cyclic_prefix:]
 chirp_suffix = chirp_sig[:cyclic_prefix]
 chirp_w_prefix_suffix = chirp_prefix + chirp_sig + chirp_suffix
@@ -39,5 +42,8 @@ overall_sig = start_sig + chirp_w_prefix_suffix + waveform
 # print(overall_sig)
 
 # Play the audio data
-sd.play(overall_sig, sample_rate)
-sd.wait()  # Wait until the sound has finished playing
+# sd.play(overall_sig, sample_rate)
+# sd.wait()  # Wait until the sound has finished playing
+
+output_file = 'audio_to_test_with.wav'
+sf.write(output_file, overall_sig, sample_rate)
