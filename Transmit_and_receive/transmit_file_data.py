2baf330eec3e0f380d86df467c4d4a8fc37a627a
Ben Domb <bd439@cam.ac.uk>
2024-05-26 14:28:10 +0100

    separated chirp and integrated in transmit

--- Transmit_and_receive/transmit_file_data.py
+++ Transmit_and_receive/transmit_file_data.py
@@ -5,6 +5,7 @@ import soundfile as sf
 from scipy.signal import chirp
 
 import parameters
+import chirp
 
 # STEP 1: encode file as binary data (e.g. LDPC)
 
@@ -20,7 +21,7 @@ binary_len = (upper_bin-lower_bin+1)*2
 symbol_count = parameters.symbol_count
 
 # WHAT IS THE REPETITION?
-coded_info_sequence = np.load("binary_data.npy")[:symbol_count*binary_len]
+coded_info_sequence = np.load("Data_files/binary_data.npy")[:symbol_count*binary_len]
 # rep_sequence = np.repeat(coded_info_sequence, repetition_factor)
 
 # STEP 2: Modulate as complex symbols using QPSK
@@ -113,24 +114,11 @@ def convert_data_to_audio(data, sampling_rate):
     # np.save("rep_waveform.npy", waveform)
     return waveform 
 
-# Chirp Parameters
-chirp_duration = parameters.chirp_duration  # seconds
-start_sig = [0]*sample_rate  # 1 second silence
-start_freq = parameters.chirp_start_freq
-end_freq = parameters.chirp_end_freq
-chirp_type = parameters.chirp_type
-cyclic_prefix = prefix_len
-
-t = np.linspace(0, chirp_duration, int(chirp_duration*sample_rate))  # time-values for chirp
-chirp_sig = chirp(t, f0=start_freq, f1=end_freq, t1=chirp_duration, method=chirp_type)
-chirp_sig = list(chirp_sig)  
-
-chirp_prefix = chirp_sig[-cyclic_prefix:]
-chirp_suffix = chirp_sig[:cyclic_prefix]
-chirp_w_prefix_suffix = chirp_prefix + chirp_sig + chirp_suffix
+chirp_sig = chirp.chirp_sig
+chirp_w_prefix_suffix = chirp.chirp_w_prefix_suffix
 
 waveform = convert_data_to_audio(concatenated_blocks, sample_rate)
-overall_sig = start_sig + chirp_w_prefix_suffix + list(waveform)
+overall_sig = chirp.start_sig + chirp_w_prefix_suffix + list(waveform)
 print(len(waveform))
 
 # Play the audio data
